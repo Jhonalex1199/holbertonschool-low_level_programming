e18cf5db345c613ebce59f76557e18766fe92c33
jhonalex1199 <2050@holbertonschool.com>
2020-06-11 03:38:11 +0000

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /**
- * main - print a string using printf
+ * main - print a string using puts
  *
  * Return: Always 0 (Success)
  */
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
