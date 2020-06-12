3cd0397a2df7218b43b263a7aecaac64d548fd30
jhonalex1199 <2050@holbertonschool.com>
2020-06-12 01:00:53 +0000

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char ch = 'a';
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch == 'e' || ch =='q')
+if (ch == 'e' || ch == 'q')
 {
 ch++;
 }
