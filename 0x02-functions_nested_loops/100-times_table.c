7724773c9af2f54f34d968adfa44ab43150053c8
jhonalex1199 <2050@holbertonschool.com>
2020-06-16 22:12:22 +0000

    c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,52 @@
 #include "holberton.h"
 
 /**
- * print_times_table - Print the n times table
- * @n: The times table to print
+ * print_times_table - prints the n times table starting with 0 between 0-15
+ * @n: Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j, k;
 
-	if ((n > 15) || (n < 0))
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n < 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			if ((i * j) < 10)
+			for (j = 0; j <= n; j++)
 			{
+				k = i * j;
 				if (j != 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					_putchar (',');
+					_putchar (' ');
+				if (k >= 10 && k < 100)
+				{
+					_putchar (' ');
+					_putchar (k / 10 + '0');
+					_putchar (k % 10 + '0');
+				}
+				else if (k < 10)
+				{
+					_putchar (' ');
+					_putchar (' ');
+					_putchar (k + '0');
+				}
+				else
+				{
+					_putchar (k / 100 + '0');
+					_putchar ((k / 10) % 10 + '0');
+					_putchar (k % 10 + '0');
 				}
 			}
-			else if ((i * j) < 100)
-			{
-				_putchar(' ');
-				_putchar(i * j / 10 + '0');
-			}
 			else
 			{
-				_putchar(i * j / 100 + '0');
-				_putchar((i * j / 10) % 10 + '0');
+				_putchar (k  + '0');
 			}
-			_putchar(i * j % 10 + '0');
-			if (j == n)
-			{
-				_putchar('\n');
 			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+		_putchar('\n');
 		}
-	}	
+	}
 }
