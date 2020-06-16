3ae1688c0a8dba46726f7a981a3e32396369c3a9
jhonalex1199 <2050@holbertonschool.com>
2020-06-16 21:49:13 +0000

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,8 @@
  */
 void print_alphabet(void)
 {
-int i = 0;
-	for (i = 97; i <= 122; i++)
-	{
+int i;
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-char ch = 0;
-int i = 0;
-        for (i = 1; i <= 10; i++)
+int a;
+int i;
+        for (i = 0; i < 10; i++)
 	{
-        	for (ch = 97; ch <= 122; ch++)
-        	{
-        		_putchar(ch);
-        	}
-		ch = 97;
+        	for (a = 'a'; a <= 'z'; a++)
+        		_putchar(a);
 		_putchar('\n');
-	} 
+	}
 }
