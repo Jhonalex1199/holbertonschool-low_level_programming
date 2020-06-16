759ed667ace8fbb605ae082123a605cfb5c8e6f3
jhonalex1199 <2050@holbertonschool.com>
2020-06-16 20:48:33 +0000

    c

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_time_table(int n);
 
-#endif
+#endif /* _HOLBERTON_H */
