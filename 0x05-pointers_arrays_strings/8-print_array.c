8f41d2aeed9a448bcfbee41f8e80c4c8cf253abf
jhonalex1199 <2050@holbertonschool.com>
2020-06-24 03:09:33 +0000

    c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,28 @@
 #include "holberton.h"
-/**
- * print_array -  prints n elements of an array of integers.
- * @a: array to print
- * @n: int the number of element
- */
+#include "stdio.h"
 
+/**
+ * print_array - print reverse characters.
+ * @a: Array.
+ * @n: Number array.
+ */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", a[i]);
-		if (i < (n - 1))
-			printf(", ");
+		for (i = 0 ; n > i; i++)
+		{
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+			       printf("%d", a[i]);
+			}
+		}
 	}
 	printf("\n");
 }
