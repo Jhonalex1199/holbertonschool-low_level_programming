530ffa218d2f9f3631179b5424de6529da6e2650
jhonalex1199 <2050@holbertonschool.com>
2020-06-24 00:51:48 +0000

    c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print other character.
+ * @str: string.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string-
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, m;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 != 0)
+	{
+		m = ((i - 1) / 2);
+	}
+	else
+	{
+		m = i / 2;
+	}
+	for (; m < i; m++)
+	{
+		_putchar (str[m]);
+	}
+	_putchar ('\n');
+}
