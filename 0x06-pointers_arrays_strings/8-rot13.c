ad696eceaa94401da476fbeac32a70e318d5e9e9
jhonalex1199 <2050@holbertonschool.com>
2020-06-27 21:02:55 +0000

    c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,17 +7,18 @@
  */
 char *rot13(char *str)
 {
-	int i = 0, k;
-	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, k;
+	char s[52] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (k = 0; k <= 51; k++)
+		for (k = 0; k < 52; k++)
 		{
-			if (s[k] == str[i])
+			if (str[i] == s[k])
 			{
 				str[i] = s1[k];
+				break;
 			}
 		}
 	}
