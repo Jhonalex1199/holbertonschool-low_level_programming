5502f59259237db7de5267f986856ab7b3ad8a6b
jhonalex1199 <2050@holbertonschool.com>
2020-07-02 06:02:30 +0000

    c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - length of a string.
+ * @s: string.
+ * Return: i.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{	
+		i = 1+ _strlen_recursion(s + 1);
+	return (i);
+	}
+	else
+	{
+		return (i);
+	}
+}
