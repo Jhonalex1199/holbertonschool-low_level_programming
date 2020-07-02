9ec34a45eac526997497c5e4bad4018675f8bffe
jhonalex1199 <2050@holbertonschool.com>
2020-07-02 22:34:55 +0000

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,21 +9,21 @@
  */
 int calraiz(int n, int i)
 {
-        if (n / i == i)
-        {
-                return (i);
-        }
-        if (n / i < i)
-        {
-                return (-1);
-        }
-        i = calraiz(n, i + 1);
-        return (i);
+	if (n / i == i)
+	{
+	return (i);
+	}
+	if (n / i < i)
+	{
+		return (-1);
+	}
+	i = calraiz(n, i + 1);
+	return (i);
 }
 /**
- * _sqrt_recursion - natural square root of a number
- * @n: number
- * Return: square root
+ * _sqrt_recursion - natural square root of a number.
+ * @n: number.
+ * Return: square root.
  */
 int _sqrt_recursion(int n)
 {
