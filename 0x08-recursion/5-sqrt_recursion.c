344e2a7b5e637e59aead85760945ff1a7c47eb5f
jhonalex1199 <2050@holbertonschool.com>
2020-07-02 22:30:53 +0000

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,34 +2,32 @@
 #include <stdio.h>
 
 /**
- * _sqrt_recursion - natural square root of a number
- * @n: number
- * Return: square root
- */
-int _sqrt_recursion(int n)
-{
-	int i = 1, res = 0;
-
-	res = calraiz(n, i);
-	return (calraiz(n, i));
-}
-
-/**
- * _sqrt_recursion - natural square root of a number.
+ * calraiz - natural square root of a number.
  * @n: number.
  * @i: divisor.
  * Return: square root.
  */
 int calraiz(int n, int i)
 {
-	if (n / i == i)
-	{
-		return (i);
-	}
-	if (n / i < i)
-	{
-		return (-1);
-	}
-	i = calraiz(n, i + 1);
-	return (i);
+        if (n / i == i)
+        {
+                return (i);
+        }
+        if (n / i < i)
+        {
+                return (-1);
+        }
+        i = calraiz(n, i + 1);
+        return (i);
+}
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: number
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	return (calraiz(n, i));
 }
