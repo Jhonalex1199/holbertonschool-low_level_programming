acb3fe19815fd6f0cf1af6f3b483e79d540e6c10
jhonalex1199 <2050@holbertonschool.com>
2020-07-18 23:57:54 +0000

    c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list p_strings;
 	unsigned int i = 0;
 	char *c;
-   
+
 	va_start(p_strings, n);
 
 	for (i = 0; i < n; i++)
