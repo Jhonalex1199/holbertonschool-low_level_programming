e6ee46a71b19005869b8467627f078c6cceece00
Jhonalex1199 <2050@holbertonschool.com>
2020-07-18 18:17:55 -0500

    c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings.
+ * @n: number arguments
+ * @separator: character separator of numbers
+ * Return: strings.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    
+}
