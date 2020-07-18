7c6c2066613c6e076211d5fa9813855fea8a358e
Jhonalex1199 <2050@holbertonschool.com>
2020-07-18 18:42:55 -0500

    c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,5 +10,24 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+    va_list p_strings;
+    unsigned int i = 0;
+    char *c;
     
+    va_start(p_strings, n);
+
+    for (i = 0; i < n; i++)
+    {
+        c = (va_arg(p_strings, char *));
+        if (c)
+            printf("%s", c);
+        if (!c)
+            printf("(nil)");
+            if (i != n - 1 && separator != NULL)
+            {
+                printf("%s", separator);
+            }
+    }
+    printf("\n");
+    va_end(p_strings);
 }
