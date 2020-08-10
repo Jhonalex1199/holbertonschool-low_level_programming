2149eaee348c3702a9f2fe65a83b3da89c162cf5
jhonalex1199 <2050@holbertonschool.com>
2020-08-10 18:14:50 -0500

    c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Function that creates a file.
+ * @filename: file name.
+ * @letters: # of letters it should read and print.
+ * Return: char.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	int letters2;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+    if (buffer == NULL)
+         return (0);
+    letters2 = read(fd, buffer, letters);
+    
+	if (write(STDOUT_FILENO, buffer, letters2) != letters2)
+            return (0);
+
+    free(buffer);
+    close(fd);
+
+	return (letters2);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_IO
+#define HOLBERTON_IO
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
