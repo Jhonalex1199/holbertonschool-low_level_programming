89a6356b26633b61bbb9970c5b9205326b78ccbe
jhonalex1199 <2050@holbertonschool.com>
2020-08-22 15:31:13 -0500

    c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - The main function.
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- * main - prints buffer in hexa
- * @argc: he number of command line arguments
- * @argv: An array containing the program command line arguments.
+ * main - print buffer.
+ * @argc: argc.
+ * @argv: argv.
  * Return: 0.
  */
 int main(int argc, char **argv)
 {
-	int i = 1, n1 = 0, n1 = 0;
+	int i, n1 = 0, n1 = 0;
 
 	if (argc != 3)
-	{
 		printf("Error\n");
-	}
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-	i = ((n1) * (n2));
-	printf("%d\n", i);
-	return (0);
+		return (1);
+	else
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		i = ((n1) * (n2));
+		printf("%d\n", i);
+		return (0);
 }
