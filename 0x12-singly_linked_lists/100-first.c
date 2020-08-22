45ec5f40f49ab9f2973f58b4dca9c2d013908583
jhonalex1199 <2050@holbertonschool.com>
2020-08-22 14:47:19 -0500

    c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * firstChain - execute the chain before the main
+ * Return: Always 0
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
