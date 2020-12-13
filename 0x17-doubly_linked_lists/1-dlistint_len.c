ca77ca3246c6db6a24486b0e128ddb3ff0edfa89
Jhonalex1199 <2050@holbertonschool.com>
2020-12-13 16:05:06 -0500

    c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,17 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function that returns the number of elements in a linked
- * dlistint_t list.
- * @h: input of a node.
- * Return: Always EXIT_SUCCESS.
- */
-
-size_t dlistint_len(const dlistint_t *h)
+ * print_dlistint - Prints all the elements.
+ * @h: node.
+ * Return: # de nodos.
+*/
+size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *tmp = h;
 	size_t i = 0;
 
-	while (tmp)
+	while (h != NULL)
 	{
-		tmp = tmp->next;
+		h = h->next;
 		i++;
 	}
 	return (i);
