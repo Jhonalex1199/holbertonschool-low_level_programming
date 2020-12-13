e448c235f23c306d67d2c8cbc41d8efe04e97190
Jhonalex1199 <2050@holbertonschool.com>
2020-12-13 18:09:10 -0500

    c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,20 +2,20 @@
 
 /**
  * add_dnodeint - function that adds a new node.
- * @*head: input of a list.
+ * @head: input of a list.
  * @n: data od the node.
  * Return: the new node at the beginning
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	//dlistint_t* primero = NULL;
-	//dlistint_t* tail = NULL;
-	dlistint_t* new;
-	new = (dlistint_t*)malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
 	new->n = n;
 	if (new == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	if (*head == NULL)
 	{
@@ -30,5 +30,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new->next = *head;
 		*head = new;
 	}
-	return(new);
+	return (new);
 }
