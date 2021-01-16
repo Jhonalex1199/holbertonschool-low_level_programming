df2afb898f40eedd4a634c28e48f5e14a0df49d5
jhonalex1199 <2050@holbertonschool.com>
2021-01-15 22:19:07 -0500

    c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,12 @@
 #include "hash_tables.h"
-
 /**
  *hash_table_create-function that creates a hash table.
  *@size: is the size of the array
  *Return: a pointer to the newly created hash table or NULL if failed
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
+
 hash_table_t *tab;
 unsigned long int x;
 
