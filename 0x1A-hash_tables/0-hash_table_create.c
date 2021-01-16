2f6b7066e78ad60cdc7c228f6cf41d8dc3a0a8d6
jhonalex1199 <2050@holbertonschool.com>
2021-01-15 22:17:06 -0500

    c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_create-function that creates a hash table.
+ *
+ *@size: is the size of the array
+ *
+ *Return: a pointer to the newly created hash table or NULL if failed
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *tab;
+unsigned long int x;
+
+	tab = malloc(sizeof(hash_table_t));
+		if (tab == NULL)
+			return (NULL);
+
+	tab->array = malloc(sizeof(hash_node_t *) * size);
+		if (tab->array == NULL)
+		{
+			free(tab);
+			return (NULL);
+		}
+
+	tab->size = size;
+	for (x = 0; x < size; x++)
+		tab->array[x] = NULL;
+
+return (tab);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ *key_index-function that gives you the index of a key
+ *
+ *@key: is the key
+ *@size: is the size of the array of the hash table
+ *
+ *Return: index of the key in the array
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned long int hash_key = hash_djb2(key);
+
+unsigned long int hash_index = hash_key % size;
+
+return (hash_index);
+}
